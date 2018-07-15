fc597295653f06e3e8b79ea9c06a0fe5e5376888
HOWARD LARRISON <development@daybreak.coffee>
2018-07-15 20:40:32 +0700

    Update strlcpy.h

--- strlcpy.h
+++ strlcpy.h
@@ -7,7 +7,7 @@
 /*
  * Copy src to string dst of size siz.  At most siz-1 characters
  * will be copied.  Always NUL terminates (unless siz == 0).
- * Returns strlen(src); if retval >= siz, truncation occurred.
+ * Returns strlen(src); if retval >= siz, truncation occurred
  */
 inline size_t strlcpy(char *dst, const char *src, size_t siz)
 {
